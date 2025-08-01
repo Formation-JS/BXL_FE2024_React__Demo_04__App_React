4ffe816026fe0e96ec7c4fca6caa0e5644fccf04
Donald Duck <aurelien.strimelle@bstorm.be>
2025-08-01 11:47:06 +0200

    Initialize démo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo 04 - Requete AJAX
+
+## Méthodes pour envoyer une requete en React :
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>Demo 04 - Requete Ajax</h1>
+    </>
+  );
+}
+
+export default App;
