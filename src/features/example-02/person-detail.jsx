55132eda4920aae30ec5079e7cbf78ab08b78569
Donald Duck <aurelien.strimelle@bstorm.be>
2025-08-01 15:32:08 +0200

    Example 2 - Cleanup Effect

--- src/features/example-02/person-detail.jsx
+++ src/features/example-02/person-detail.jsx
@@ -8,6 +8,9 @@ export default function PersonDetail({ personId }) {
     const [messageError, setMessageError] = useState(null);
 
     useEffect(() => {
+        //! Execution de la requete via un effect
+        let isCancel = false;
+
         setLoading(true);
         setPerson(null);
         setMessageError(null);
@@ -15,13 +18,28 @@ export default function PersonDetail({ personId }) {
         fetch('http://localhost:3000/people/' + personId)
             .then(response => response.json())
             .then(data => {
+                //? Si le "cleanup" a été déclancher, on active pas la suite du code
+                if(isCancel) return;
+
                 setPerson(data)
                 setLoading(false)
             })
             .catch(error => {
+                //? Si le "cleanup" a été déclancher, on active pas la suite du code
+                if(isCancel) return;
+                
                 setMessageError(error.message)
                 setLoading(false)
-            })
+            });
+
+        return () => {
+            //! Cleanup de l'effet
+            isCancel = true;   
+
+            //? Tips : Possibilité d'annuler la requete via les options du "fetch"
+            //  https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+        }
+
     }, [personId])
     
     return (
